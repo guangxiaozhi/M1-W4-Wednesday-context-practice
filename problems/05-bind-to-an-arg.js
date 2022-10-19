1ddfc45031ab15131aab9b1d6aeea94e728b01fc
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-10-19 14:50:25 -0700

    bind again

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,7 +1,8 @@
 function bindToAnArg(func, arg) {
-  
+
   return func.bind(null, arg);
- 
+  // return func.bind(bindToAnArg, arg);
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
