3da5e104a19640faae4587b0a46c9011e1684110
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-10-19 14:23:18 -0700

    04

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,7 +1,24 @@
 function changeContext(func, obj) {
   // Your code here
+  // return func.call(obj)
+  // let banana = func.bind(obj);
+  // return banana(obj);
+  return func.apply(obj);
 }
 
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
